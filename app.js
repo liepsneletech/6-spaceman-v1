c9bf771c4b18fdc1326135751243e98dd73f8064
liepsnele <liepsnele.tech@gmail.com>
2022-09-30 13:57:16 +0300

    Added collapsed navbar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "strict";
 
-const headerNav = document.querySelector(".header-nav");
+const headerNav = document.querySelector(".header");
 const mobileMenuBtns = document.querySelector(".mobile-menu-btns");
 const mobileBtnHam = document.querySelector(".mobile-btn-ham");
 const mobileBtnX = document.querySelector(".mobile-btn-x");
