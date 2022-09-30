5d292ccc3daf69adf94f30e72127e2583babc842
liepsnele <liepsnele.tech@gmail.com>
2022-09-30 13:21:38 +0300

    Remade without table and table-cells

---
+++ app.js
@@ -0,0 +1,12 @@
+"strict";
+
+const headerNav = document.querySelector(".header-nav");
+const mobileMenuBtns = document.querySelector(".mobile-menu-btns");
+const mobileBtnHam = document.querySelector(".mobile-btn-ham");
+const mobileBtnX = document.querySelector(".mobile-btn-x");
+
+mobileMenuBtns.addEventListener("click", function () {
+  headerNav.classList.toggle("show");
+  mobileBtnX.classList.toggle("show");
+  mobileBtnHam.classList.toggle("hide");
+});
